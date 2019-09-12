7257fc98baef6ff132a8a1304ac9b45e927b0c4d
nickmoran06 <956@holbertonschool.com>
2019-09-12 20:00:36 +0000

    [Fix] have into account negative numbers

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     len_arg = len(argv)
 
-    if len_arg == 1:
+    if len_arg <= 1:
         print("{:d} {:s}{:s}".format(len_arg - 1, "argument", "."))
     elif len_arg == 2:
         print("{:d} {:s}{:s}".format(len_arg - 1, "argument", ":"))
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     for i, j in enumerate(argv):
         if i > 0:
-            print("{}: {}".format(i, j))
+            print("{:d}: {:s}".format(i, j))
