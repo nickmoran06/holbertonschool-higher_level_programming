01779a9ffb5e6f256465670dcb6a268c91d7bc40
nickmoran06 <956@holbertonschool.com>
2019-09-17 05:05:45 +0000

    [feature] function that verify a cycle in a linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ *
+ * @list: pointer to the first node
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *single = NULL, *doub = NULL;
+
+	single = list;
+	doub = list;
+	if (!list)
+		return (0);
+	while (single->next && doub->next->next)
+	{
+		single = single->next;
+		doub = doub->next->next;
+		if (single == doub)
+			return (1);
+	}
+
+	return (0);
+}
