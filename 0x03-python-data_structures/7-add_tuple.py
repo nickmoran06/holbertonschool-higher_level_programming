a9ef836085e2e8c4713f44577bc43738c501d126
nickmoran06 <956@holbertonschool.com>
2019-09-18 22:51:00 +0000

    [Update] changing order

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    a = len(tuple_a)
-    b = len(tuple_a)
     tuple_a += (0, 0)
     tuple_b += (0, 0)
+    tuple_c = ()
 
-    if a is 2 and b is 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif a < 2 and b is 2:
-        return tuple_b
-    elif b < 2 and a is 2:
-        return tuple_a
-    elif a is 0 and b is 0:
-        return tuple_a
-    elif a < 2 and b is 0:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif a < 2 and b is 0:
-        return (tuple_a[0] + tuple_b[0], 0)
-    elif a is 0 and b < 2:
-        return (0, tuple_a[1] + tuple_b[1])
+    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tuple_c
