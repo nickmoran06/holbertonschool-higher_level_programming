c9db3dda00efec704be4701d3df2fb74e7416ab3
nickmoran06 <956@holbertonschool.com>
2019-09-18 22:43:45 +0000

    [Update] improving conditional

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,19 +7,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_b += (0, 0)
 
     if a is 2 and b is 2:
-        pos_0 = tuple_a[0] + tuple_b[0]
-        pos_1 = tuple_a[1] + tuple_b[1]
-        tuple_c = (pos_0, pos_1)
-        return tuple_c
-    elif a < 2:
-        tuple_a[1] = 0
-    elif b < 2:
-        tuple_b[1] = 0
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     elif a < 2 and b is 2:
         return tuple_b
     elif b < 2 and a is 2:
         return tuple_a
     elif a is 0 and b is 0:
         return tuple_a
-    elif a < 2 and b < 2:
+    elif a < 2 and b is 0:
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    elif a < 2 and b is 0:
         return (tuple_a[0] + tuple_b[0], 0)
+    elif a is 0 and b < 2:
+        return (0, tuple_a[1] + tuple_b[1])
