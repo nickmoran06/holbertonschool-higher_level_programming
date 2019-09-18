fbf70629d588febed38c461bda1f48ca95c41d5f
nickmoran06 <956@holbertonschool.com>
2019-09-18 04:37:51 +0000

    [Update] improve temporal variable

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        tmp = 0
+        tmp = my_list[0]
         for counter in range(len(my_list)):
             if my_list[counter] > tmp:
                 tmp = my_list[counter]
