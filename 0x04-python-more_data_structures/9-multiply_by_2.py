9ad9efa6ed216d5f513d8e0d5cdf83a2878708ea
nickmoran06 <956@holbertonschool.com>
2019-09-19 23:11:01 +0000

    [Update] create a copy

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for counter in a_dictionary:
-        a_dictionary[counter] *= 2
-    return a_dictionary
+    b_dictionary = a_dictionary.copy()
+    for counter in b_dictionary:
+        b_dictionary[counter] *= 2
+    return b_dictionary
