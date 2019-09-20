65696b8dbcadfdd79459cc25a90bc624529a2dca
nickmoran06 <956@holbertonschool.com>
2019-09-20 19:06:10 +0000

    [Update] improve list comprenhension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def counter_selec(counter):
-        if counter != search:
-            return counter
-        return replace
-    return list(map(counter_selec, my_list))
+    return list(map(lambda counter: counter if counter != search else replace, my_list))
