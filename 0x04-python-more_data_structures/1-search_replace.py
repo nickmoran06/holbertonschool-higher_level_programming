4a6ec1a4a505e59625e7e1ba8bf3bc53a72178ee
nickmoran06 <956@holbertonschool.com>
2019-09-20 19:07:20 +0000

    [Update] pep8

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return list(map(lambda counter: counter if counter != search else replace, my_list))
+    return list(map(lambda c: c if c != search else replace, my_list))
