68c5d77a37d2e808e0567a10a2434cf1f9fa4517
nickmoran06 <956@holbertonschool.com>
2019-09-30 20:00:02 +0000

    [Feature] function that divide two lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    division_list = []
+
+    for counter in range(list_length):
+        try:
+            division = my_list_1[counter] / my_list_2[counter]
+        except TypeError:
+            print("wrong type")
+            division = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            division = 0
+        except IndexError:
+            print("out of range")
+            division = 0
+        finally:
+            division_list.append(division)
+    return(division_list)
