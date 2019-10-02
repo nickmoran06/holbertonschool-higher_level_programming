dfac1fd605e9c73832f45352840feecae0b4f1f5
nickmoran06 <956@holbertonschool.com>
2019-10-02 20:24:53 +0000

    [Feature] class with the method of print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def my_print(self):
+        if self.__size is 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
