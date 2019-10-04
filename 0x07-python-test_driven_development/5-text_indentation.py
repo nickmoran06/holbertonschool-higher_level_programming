c2de152415afaaa6a7da82ea9358fd64e0cba050
nickmoran06 <956@holbertonschool.com>
2019-10-04 21:12:38 +0000

    [Feature] Function to indent a text and his test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    text = text.replace(". ", ".\n\n")
+    text = text.replace("? ", "?\n\n")
+    text = text.replace(": ", ":\n\n")
+
+    print("{:s}".format(text))
