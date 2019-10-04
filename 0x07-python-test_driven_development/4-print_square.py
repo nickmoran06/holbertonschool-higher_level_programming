287ef90973ca53e295cbffe8b1824a8efa885367
nickmoran06 <956@holbertonschool.com>
2019-10-04 18:24:17 +0000

    [Feature] function to print a square and the test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif size is 0:
+        pass
+    else:
+        for counter in range(size):
+            print("#" * size)
