aec21bb3c9cb897144c394fa4a08dc66a0965096
nickmoran06 <956@holbertonschool.com>
2019-10-12 02:00:58 +0000

    [Fix] reorganizing the program

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 def text_indentation(text):
+    sym = [".", "?", ":"]
+    ch = 0
+
     if type(text) is not str:
         raise TypeError("text must be a string")
-    text = text.replace(". ", ".\n\n")
-    text = text.replace("? ", "?\n\n")
-    text = text.replace(": ", ":\n\n")
-    text = text.replace(".", ".\n\n")
-    text = text.replace(":", ":\n\n")
-    text = text.replace("?", "?\n\n")
-    print("{:s}".format(text))
+
+    while ch in range(len(text)):
+        if text[ch] is sym[0] or text[ch] is sym[1] or text[ch] is sym[2]:
+            print("{:s}".format(text[ch]) + "\n")
+            ch += 1
+            while ch in range(len(text)):
+                if text[ch] is not " ":
+                    break
+                else:
+                    ch += 1
+        else:
+            print("{:s}".format(text[ch]), end="")
+            ch += 1
