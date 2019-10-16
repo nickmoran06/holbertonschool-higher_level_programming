a5af8fe3c7974f3da5464406be2e7691ccd64d4a
nickmoran06 <956@holbertonschool.com>
2019-10-16 17:40:03 +0000

    [Update] changing methodology, adding area and alias

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,7 +5,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
+    def area(self):
+        return super().area()
+
     def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
