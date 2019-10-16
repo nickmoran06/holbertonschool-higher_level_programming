cd4dc04b8f5413cf4204a809b4f40cb7748f81eb
nickmoran06 <956@holbertonschool.com>
2019-10-16 22:25:17 +0000

    [Feature] Function to return that creates an Object from JSON file

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+
+
+def load_from_json_file(filename):
+    """Function that creates an Object from a “JSON file”"""
+    with open(filename, "r", encoding="utf-8") as TxtFile:
+        return json.load(TxtFile)
