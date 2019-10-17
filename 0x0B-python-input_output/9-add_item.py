f9c51b37964f71fd19e444e01e035cb0f1eee92a
nickmoran06 <956@holbertonschool.com>
2019-10-17 00:53:53 +0000

    [Update] changing order

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+from sys import argv
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
-from sys import argv
 
-
-filename = "add_item.json"
 
 try:
-    json_list = load_from_json_file(filename)
+    json_list = load_from_json_file("add_item.json")
 except:
     json_list = []
 
 for arguments in argv[1:]:
     json_list.append(arguments)
 
-save_to_json_file(json_list, filename)
+save_to_json_file(json_list, "add_item.json")
