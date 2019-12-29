527ab656d53c392ab3710c769769bfa42c56496c
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-12-29 05:38:22 +0000

    [Feature] function to insert a node in a ordered linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a new node at a given position.
+ *
+ * @head: first node od the list
+ * @number: index to insert a number
+ *
+ * Return: the address of the new node,
+ * or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = NULL;
+	listint_t *cpy = NULL;
+	listint_t *ref = NULL;
+
+
+	if (!head)
+		return (0);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+
+	if (!*head)
+	{
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+
+	new->n = number;
+	cpy = *head;
+	while (cpy)
+	{
+		if (cpy->n < number)
+		{
+			ref = cpy;
+			cpy = cpy->next;
+		}
+		else
+			break;
+	}
+	new->next = ref->next;
+	ref->next = new;
+
+	return (new);
+}
