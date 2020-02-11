2b5a62fa3669508008a45e4d79bfcee70ea502b0
nickmoran06 <956@holbertonschool.com>
2020-02-11 19:43:33 +0000

    [Fix] Way to print the error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-    return;
+    console.log(err)
   }
   console.log(data);
 });
