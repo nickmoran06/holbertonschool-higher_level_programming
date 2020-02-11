ded9082e8fae6a53d40a7f6e01cc29022f637ab1
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-02-11 04:02:48 +0000

    [Feature] Method to print C

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let count = 0; count < this.width; count++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
