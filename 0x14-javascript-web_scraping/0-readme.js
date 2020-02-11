1b3b9d061bca74a1b1d4d16e5ae5490863deec72
nickmoran06 <956@holbertonschool.com>
2020-02-11 19:46:11 +0000

    [Fix] addition of else

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
