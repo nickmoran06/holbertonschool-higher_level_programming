f6c1d502405b0fcda91267829dc5368e1aa59910
nickmoran06 <956@holbertonschool.com>
2020-02-11 19:40:55 +0000

    [Feature] Change the way to read the file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
-try {
-  const data = fs.readFileSync(process.argv[2], 'utf8')
-  console.log(data)
-} catch (err) {
-  console.error(err)
-}
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
